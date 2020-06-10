35505377b994e73dc549f549f5fa5ce5130abd4a
Damn_Imposter <33062172+Impo5ter@users.noreply.github.com>
2020-06-10 14:02:47 +0400

    Add stady menu and button back

--- app/src/main/java/com/example/appforbeginnerguitarists/MainActivity.java
+++ app/src/main/java/com/example/appforbeginnerguitarists/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.appforbeginnerguitarists;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button buttonStudy = (Button)findViewById(R.id.Study);
+        buttonStudy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Intent intent = new Intent(MainActivity.this, Subjects.class);
+                    startActivity(intent);finish();
+                }catch (Exception e){
+
+                }
+            }
+        });
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
